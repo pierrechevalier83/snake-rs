bf178ad98db3e28589d99060a406ba51d0a53ea5
Pierre Chevalier <pierrechevalier83@gmail.com>
2017-03-29 18:16:12 +0100

    Implement snake's data representation

--- src/main.rs
+++ src/main.rs
@@ -19,9 +19,32 @@ impl Board {
 	}
 }
 
+#[derive(Clone)]
+enum Direction {
+    up,
+	down,
+	left,
+	right,
+}
+
+struct Snake {
+    heads_index: usize,
+	body: Vec<Direction>,
+}
+
+impl Snake {
+    fn new(size: usize) -> Snake {
+	    Snake {
+		    heads_index: 0,
+			body: vec![Direction::left; size],
+		}
+	}
+}
+
 fn main() {
     let format = Format::new(3, 1);
     let board = Board::new(22);
+	let snake = Snake::new(4);
     let data = matrix::Matrix::new(board.n_cols(), board.data());
     let display = MatrixDisplay::new(format, data);
     display.print(&mut std::io::stdout(), &style::BordersStyle::None);
