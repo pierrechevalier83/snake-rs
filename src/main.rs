697f1429ad46e6af65f66fbf1931d05a9aee7ffe
Pierre Chevalier <pierrechevalier83@gmail.com>
2017-03-29 17:56:44 +0100

    Print canvas

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+extern crate matrix_display;
+use matrix_display::*;
+
 fn main() {
-    println!("Hello, world!");
+    let format = Format::new(3, 1);
+    let n_cols = 22 as usize;
+    let board = (0..n_cols * n_cols).map(|_| cell::Cell::new(" ", 0, 16)).collect::<Vec<_>>();
+    let data = matrix::Matrix::new(n_cols, board);
+    let display = MatrixDisplay::new(format, data);
+    display.print(&mut std::io::stdout(), &style::BordersStyle::None);
 }
