58f8cd27bdabaedffac4087f78ff8036f1582e02
Pierre Chevalier <pierrechevalier83@gmail.com>
2017-08-13 11:05:43 +0100

    Only redraw changed lines (fixes glitches with st)

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 extern crate matrix_display;
+extern crate ansi_term;
 extern crate num;
 extern crate rand;
 extern crate termion;
@@ -22,23 +23,38 @@ use game::Game;
 use game::Status;
 use tui::Terminal;
 use tui::backend::TermionBackend;
+use tui::widgets::{Block, Paragraph, Widget};
+use tui::layout::{Group, Size};
 
-fn print_game<W>(game: &Game, stdout: &mut W, terminal: &mut Terminal<TermionBackend>)
+fn print_game<W>(game: &Game, stdout: &mut W, terminal: &mut Terminal<TermionBackend>, previous_string: &mut String)
     where W: Write
 {
     let mut data = matrix::Matrix::new(game.n_cols() as usize, game.board());
     let format = Format::new(3, 1);
     let display = MatrixDisplay::new(&format, &mut data);
-    terminal.clear().unwrap();
-    terminal.hide_cursor().unwrap();
-    write!(stdout, "{}Score: {} \r\n", termion::color::Bg(termion::color::AnsiValue(233)), game.score()).unwrap();
-    display.print(stdout, &style::BordersStyle::None);
-    stdout.flush().unwrap();
+    let raw = display.render(&style::BordersStyle::None);
+    let new_string = format!("{}Score: {}\r\n{}", termion::color::Bg(termion::color::AnsiValue(233)), game.score(), ansi_term::ANSIStrings(&*raw));
+    if *previous_string == String::new() {
+        *previous_string = new_string.lines().map(|_| { "\r\n" } ).collect();
+    }
+    write!(stdout, "{}\r\n", termion::cursor::Goto(1,1)).unwrap();
+    new_string
+        .lines()
+        .zip(previous_string.lines())
+        .map(|(new, old)| {
+            if new == old {
+                write!(stdout, "\r\n").unwrap();
+            } else {
+                write!(stdout, "{}\r\n", new).unwrap();
+            }
+        })
+        .collect::<Vec<_>>();
+    *previous_string = new_string.clone();
 }
 
 fn pick_a_size(terminal: &Terminal<TermionBackend>) -> isize {
     match terminal.size() {
-        Ok(r) => std::cmp::min((r.width / 3) as isize, std::cmp::min(r.height as isize, 40)),
+        Ok(r) => std::cmp::min((r.width / 3) as isize, (r.height - 4) as isize),
         Err(_) => 20,
     }
 }
@@ -47,9 +63,13 @@ fn main() {
     let mut terminal = Terminal::new(TermionBackend::new().unwrap()).unwrap();
     let mut stdin = termion::async_stdin().events();
     let mut stdout = stdout().into_raw_mode().unwrap();
+    let mut previous_string = String::new();
+    terminal.clear().unwrap();
+    terminal.hide_cursor().unwrap();
 
     let mut game = Game::new(pick_a_size(&terminal));
-    print_game(&game, &mut stdout, &mut terminal);
+    terminal.clear().unwrap();
+    print_game(&game, &mut stdout, &mut terminal, &mut previous_string);
     let mut direction = Direction::Right;
     let mut speed = 100;
     loop {
@@ -98,7 +118,7 @@ fn main() {
             Status::Hungry => (),
         };
         game.refresh();
-        print_game(&game, &mut stdout, &mut terminal);
+        print_game(&game, &mut stdout, &mut terminal, &mut previous_string);
         std::thread::sleep(std::time::Duration::from_millis((10000 / speed) as u64));
     }
 }
